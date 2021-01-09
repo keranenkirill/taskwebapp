674aa6b9c5845884afb5e036cc2ad60ef60895db
Kirill Keränen <keranen.kirill@gmail.com>
2021-01-09 19:03:33 +0200

    part of code copied from master for later changes and additions

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+//function to add new task in list
+function addElement(){
+    //creating list element
+    var li = document.createElement("li");
+    //storing input field value into variable 
+    var inputValue = document.getElementById("input").value;
+    //creating text node for adding to list element
+    var text = document.createTextNode(inputValue);
+
+    //list element appends text node value
+    li.appendChild(text);
+    //adding list element in unordered list element
+    document.getElementById("mylist").appendChild(li);
+    //updating textfield value to none
+    document.getElementById("input").value = "";
+
+
+}
